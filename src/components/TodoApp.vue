ec1cb09a0e4beeff93ac7b478464f5b36f3179b3
WojtekB <wojtek@vosfactures.fr>
2023-05-24 16:13:42 +0200

    todo list app

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TodoForm from '@/components/TodoForm.vue'
+import TodoList from '@/components/TodoList.vue'
+</script>
+
+<template>
+  <div class="todo-app">
+    <h1>To do list</h1>
+    <todo-form/>
+    <todo-list/>
+  </div>
+</template>
+
+<style scoped>
+.todo-app {
+  padding: 5vh;
+  min-height: 100vh;
+  width: 30vw;
+  background-color: #f7f9fc;
+}
+</style>
